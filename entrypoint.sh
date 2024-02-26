c6874ac4653b13af1ae283bae39b31376025886a
LuciusCornelius88 <volodymir.melnyk@gmail.com>
2024-02-26 14:29:50 +0200

    deleted comments in entrypoint scrypt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,3 @@
-#!/bin/bash
-# set -e
-
-# host="mysql"
-# port="3306"
-# user="root"
-# password="password"
-
-# >&2 echo "Waiting for MySQL to be available..."
-
-# for i in {1..250}; do
-#     if mysqladmin ping -h "$host" -P "$port" -u "$user" -p"$password" &> /dev/null; then
-#         >&2 echo "MySQL is available - executing app"
-        # flask db init
-        # flask db migrate
-        # exec flask run
-#     else
-#         >&2 echo "MySQL is unavailable - sleeping for 10 seconds"
-#         sleep 10
-#         flask db init
-#         flask db migrate
-#         exec flask run
-#     fi
-# done
-
-# >&2 echo "Maximum retries exceeded. Exiting."
-# exit 1
-
-
-
 attempts=0
 max_attempts=100
 sleep_time=5
